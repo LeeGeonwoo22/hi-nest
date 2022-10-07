261b94d5aeec3cc071979333c6102326334506e1
Geon Woo <kwoo.chris@gmail.com>
2022-10-07 20:12:05 +0900

    #2 노마드코더 restAPI 강의

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   // url을 가져오고 함수를 실행함. express의 router 같은 존재
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
